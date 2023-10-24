fba38b5b28d88aa06289289fbe4dbfd754319bcd
Michael W. <michael@kemmejer.de>
2023-10-24 23:18:31 +0200

    Replaced pipenv with setup.bat

---
+++ setup.bat
@@ -0,0 +1,8 @@
+python -m pip install --upgrade pip
+pip install virtualenv
+python -m venv .venv
+.venv\Scripts\activate && ^
+python -m pip install --upgrade pip && ^
+pip install torch~=1.13.1 -f https://download.pytorch.org/whl/torch_stable.html && ^
+pip install -e "Dependencies/ml-agents-envs" && ^
+pip install -e "Dependencies/ml-agents"

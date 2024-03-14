a88fb0e76efa5c3138d0d37b5d969e818dbdb46f
Michael W. <michael@kemmejer.de>
2024-03-14 11:59:04 +0100

    Added a delay to opening the browser in openboard.bat

--- openboard.bat
+++ openboard.bat
@@ -1,6 +1,7 @@
 @cd /D "%~dp0"
 
-start "" "http://localhost:6006/"
+start "" /b cmd /c "timeout /nobreak 4 >nul & start "" http://localhost:6006/"
+
 cd scripts
 .venv\Scripts\activate && ^
 tensorboard --logdir ../results --port 6006 --reload_interval=30 --reload_multifile=true --samples_per_plugin images=100 & ^
